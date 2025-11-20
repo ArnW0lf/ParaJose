5496ceae2dadb97b908b6369ed0bcb23e27d37a5
Arn0lf <perrotricefalo@gmail.com>
2025-11-19 21:15:41 -0400

    first commit3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import GeneratorForm from './components/GeneratorForm';
 import PublicationCard from './components/PublicationCard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// API_BASE_URL = 'http://127.0.0.1:8000/api';
+//const API_BASE_URL = 'http://127.0.0.1:8000/api';
 const API_BASE_URL = 'https://backendsito.onrender.com/api';
 
 function App() {
